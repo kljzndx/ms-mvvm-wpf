609d4e8d1cf381e3f07307c2d5c994276d9a074f
李继龙 <kljzndx@outlook.com>
2022-02-18 15:27:36 +0800

    编写Demo项目

---
+++ HappyStudio.Mvvm.Demo/ViewModels/CommandTestViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+using HappyStudio.Mvvm.Input.Wpf;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyStudio.Mvvm.Demo.ViewModels
+{
+    public class CommandTestViewModel : ObservableRecipient
+    {
+        private bool _isAButton;
+
+        public CommandTestViewModel()
+        {
+            AButtonCommand = new RelayCommand(() => _isAButton = true, () => !_isAButton);
+            BButtonCommand = new RelayCommand(() => _isAButton = false, () => _isAButton);
+        }
+
+        public RelayCommand AButtonCommand { get; }
+        public RelayCommand BButtonCommand { get; }
+
+    }
+}
